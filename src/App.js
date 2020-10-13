105d55974dda643a559a5e72636ad9f7edab30f4
Mahyar Jamalabadi <mahyar.jamalabadi@gmail.com>
2020-10-13 19:02:51 +0330

    Add ShowImage functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './App.module.sass';
 import useFetchTrees from './hooks/useFetchTrees'
 import Card from './components/Card'
+import TreeBox from './components/TreeBox'
 
 const App = () => {
   const { data, loading } = useFetchTrees();
@@ -10,18 +11,19 @@ const App = () => {
   return (
     <div>
       <header className={styles.header}>
-        <h1>
-          Baobab Tree Showcase
-        </h1>
+        <h1>Baobab Tree Showcase</h1>
       </header>
       <main className={styles.main}>
         {loading
           ? <p>Loading...</p>
-          : data?.trees?.map((tree, index) => {
+          : data?.trees?.map(({name, species_name, image}, index) => {
             return (
               <Card key={index}>
-                <h3>{tree.name}</h3>
-                <h6>{tree.species_name}</h6>
+                <TreeBox
+                  title={name}
+                  subtitle={species_name}
+                  image={image}
+                />
               </Card>
             )
           })}
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Button.module.sass';
+
+const Button = (props) => {
+  const { label, onClick } = props;
+  return <section className={styles.container} onClick={onClick}>{label}</section>;
+};
+
+export default Button;
